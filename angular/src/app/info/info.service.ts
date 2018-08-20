2a9514298fa0d272242b79244420f017afa3b8bb
tilana <NatalieWidmann@gmx.de>
2018-08-21 00:10:58 +0200

    correct path to interface template

--- angular/src/app/info/info.service.ts
+++ angular/src/app/info/info.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CollectionInfo } from './info.model';
+import { CollectionInfo } from './info.interface';
 //import { map } from 'rxjs/operators';
 
 @Injectable()

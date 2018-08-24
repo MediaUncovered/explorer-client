1375c01df08f984f33c82918fdaa53c3635de70f
tilana <NatalieWidmann@gmx.de>
2018-08-24 11:09:34 +0200

    standardize adn improve user interface

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Media Uncovered Explorer';
+  title = 'Media Uncovered';
   serverName = 'default';
 
 }

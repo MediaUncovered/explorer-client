68586d0eb9a45f4d4760460b1ebaa2e251db5b22
tilana <NatalieWidmann@gmx.de>
2018-08-31 14:19:57 +0200

    fix reference to API URL

--- angular/src/app/info/info.service.ts
+++ angular/src/app/info/info.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CollectionInfo } from './info.interface';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class InfoService {
@@ -8,7 +9,7 @@ export class InfoService {
 	constructor(private http: HttpClient) { }
 
 	getCollectionInfo() {
-		return this.http.get<CollectionInfo>("environment.API_URL" + 'info');
+		return this.http.get<CollectionInfo>(environment.API_URL + 'info');
 	}
 
 }

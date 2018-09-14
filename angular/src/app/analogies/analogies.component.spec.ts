feb97b567c2caed5594b00640b156ae1c690ffd5
tilana <NatalieWidmann@gmx.de>
2018-09-14 10:12:25 +0200

    set up basic testing environment

--- angular/src/app/analogies/analogies.component.spec.ts
+++ angular/src/app/analogies/analogies.component.spec.ts
@@ -1,25 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 
 import { AnalogiesComponent } from './analogies.component';
+import { AnalogyService } from '../data/analogy.service';
+import { Analogy } from '../data/analogy.interface';
+
 
 describe('AnalogiesComponent', () => {
+
   let component: AnalogiesComponent;
+  let service: AnalogyService;
   let fixture: ComponentFixture<AnalogiesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AnalogiesComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AnalogiesComponent],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule
+      ],
+      providers: [
+        AnalogyService,
+      ]  
+    });
     fixture = TestBed.createComponent(AnalogiesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.debugElement.componentInstance;
+    service = TestBed.get(AnalogyService);
   });
+  
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have two analogies', () =>{
+    const mock = [new Analogy(), new Analogy()];
+    spyOn(service, 'getAnalogies').and.returnValue(of(mock));
+    fixture.detectChanges();
+    component.generateAnalogies();
+    expect(component.analogies.length).toBe(2);
+  });
+
 });
---
+++ angular/src/app/info/info.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, async, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+//import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { InfoComponent } from './info.component';
+import { InfoService } from './info.service';
+
+describe('Component: Info', () => {
+
+	let infoComponent: InfoComponent;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [InfoComponent],
+			imports: [HttpClientTestingModule],
+			providers: [InfoService],
+		}).compileComponents();
+		const fixture = TestBed.createComponent(InfoComponent);
+		infoComponent = fixture.componentInstance;
+	});
+
+	it('should create the app', () => {
+		expect(infoComponent).toBeTruthy();
+	});
+
+	it('should not have information about a collection', () => {
+		expect(infoComponent.hasInfo).toBeFalsy;
+	})
+
+});

3f12a5a5b0d7f10e046a62b99c6ce0f78938bb0e
tilana <NatalieWidmann@gmx.de>
2018-09-29 17:14:25 +0200

    code cleanup and styling

--- angular/src/app/info/data/reliability.interface.ts
+++ angular/src/app/info/data/reliability.interface.ts
@@ -2,4 +2,5 @@ export interface Reliability{
 	section: string;
 	nr_total: number;
 	nr_correct: number;
+	percentage?: number;
 }
--- angular/src/app/info/reliability/reliability.component.ts
+++ angular/src/app/info/reliability/reliability.component.ts
@@ -14,29 +14,17 @@ import { InfoService } from '../data/info.service';
 
 export class ReliabilityComponent implements OnInit{
 
-	accuracies: Reliability[] = []; 
+	accuracies: Reliability[] = [];
 
 	constructor(private service: InfoService) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.getReliabilities()
+	}
 
 	getReliabilities(): void {
 		this.service.getReliability()
 			.subscribe(data => this.accuracies = data);
-		this.log();
-	}
-
-	getFakeReliabilities(): void{
-		this.accuracies = [{section:'f', n_correct:1, n_total:1}, {section:'s', n_correct:2, n_total:2}];
-		this.log();
-	}
-
-	log(): void {
-		console.log(this.accuracies)
-		console.log(this.accuracies.length)
-		for (let accuracy of this.accuracies) {
-			console.log(accuracy.section);
-		}
 	}
 
 }

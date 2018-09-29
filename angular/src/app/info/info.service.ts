0ce86170423bafb48c37a7e7f3c1bf82924c3c69
tilana <NatalieWidmann@gmx.de>
2018-09-29 08:36:56 +0200

    combine info services to get model, collection and reliability information

--- angular/src/app/info/info.service.ts
+++ angular/src/app/info/info.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { CollectionInfo } from './info.interface';
 import { environment } from '../../environments/environment';
 
+import { ModelInfo } from './model-info.interface';
+import { CollectionInfo } from './collection-info.interface';
+import { Reliability } from './reliability.interface';
+
 @Injectable()
-export class InfoService {
+export class InfoService{
 
 	constructor(private http: HttpClient) { }
+
+	getModelInfo() {
+		return this.http.get<ModelInfo>(environment.API_URL + 'modelInfo');
+	}
 
 	getCollectionInfo() {
 		return this.http.get<CollectionInfo>(environment.API_URL + 'info');
 	}
 
+	getReliability(): Observable<Reliability[]> {
+		const url = environment.API_URL + 'reliability';
+		return this.http.get<Reliability[]>(url);
+	}
 }

45a34d8561f4e786561f84b32af8650379258881
tilana <NatalieWidmann@gmx.de>
2018-10-04 12:18:44 +0200

    add default popover elements to components at the explore page

--- angular/src/app/info/info-navigation.component.ts
+++ angular/src/app/info/info-navigation.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-info-navigation',
   templateUrl: './info-navigation.component.html',
+	styleUrls: ['./info-navigation.component.css']
 })
 export class InfoNavigationComponent implements OnInit {
 
